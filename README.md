b8e1ba6de5ad1898418acfbdf67de9086a02e898
vincentl <344166929@qq.com>
2016-11-03 14:45:59 +0800

    ···

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 
 
 课程地址：http://study.163.com/course/courseMain.htm?courseId=1002794001#/courseDetail
+
+1 **xiaozhu.py** 爬取小猪短租深圳前3页的房源标题和价格存入Mongodb,并打印出价格大于500的
--- xiaozhu.py
+++ xiaozhu.py
@@ -1,20 +1,34 @@
 from bs4 import BeautifulSoup
 import requests
+import pymongo
 
-
-
+client = pymongo.MongoClient('localhost', 27017)
+xiaozhu = client['xiaozhu']
+prices_tab = xiaozhu ['prices']
 
 def GetUrl(page=3):
 	urls = []
 	for i in range(1, page + 1):
-		url='http://sz.xiaozhu.com/search-duanzufang-p{}-0/'.format(str(page))
+		url='http://sz.xiaozhu.com/search-duanzufang-p{}-0/'.format(str(i))
 		urls.append(url)
-	print(urls)
+	return urls
+
+def GetPrices():
+	url_list = GetUrl()
+	for url in url_list:
+		wb_data = requests.get(url)
+		soup = BeautifulSoup(wb_data.text, 'lxml')
+		prices = soup.select('span.result_price i')
+		titles = soup.select('span.result_title')
+		for price, title in zip(prices, titles):
+			data = {
+				'price': int(price.get_text()),
+				'title': title.get_text()
+			}
+			prices_tab.insert_one(data)
 
 
-GetUrl()
-
-
+GetPrices()
 
 
 
