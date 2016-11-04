f3720048098a4efd03b96d9fedb96d06f733f45f
vincentl <344166929@qq.com>
2016-11-04 19:04:42 +0800

    正文提取

--- wenhui.py
+++ wenhui.py
@@ -1,6 +1,16 @@
 from bs4 import BeautifulSoup
-import requests
 from datetime import *
+import requests
+import pymongo
+
+dt = datetime.now()
+today = (str(dt.strftime('%Y%m%d')))
+
+client = pymongo.MongoClient('localhost', 27017)
+wenhui = client['wenhui']
+finance = wenhui['finance']
+
+finance_url = 'http://news.wenweipo.com/list_srh.php?date=20161104&cat=000IN&instantCat=finance'
 
 
 
@@ -33,12 +43,30 @@ def Get_info():
 	for url in urls:
 		wb_data = requests.get(url)
 		soup =  BeautifulSoup(wb_data.content, 'lxml')
-		title = soup.select('h1.title')
-		author = soup.select('p.fromInfo')
-		time = soup.select('span.date')
-		source = soup.select('span.current')
-		a = soup.select('#main-content p')
-		print(title,author,time,source)
+		titles = soup.select('h1.title')
+		authors = soup.select('p.fromInfo')
+		dates = soup.select('span.date')
+		sources = soup.select('span.current')
+		times = Get_time(finance_url)
+		text = soup.select('#main-content p:nth-of-type(old)')
+		print(text)
+		for title, author, date, source, time in zip(titles, authors, dates, sources, times):
+			data = {
+				'title': title.get_text(),
+				'author': author.get_text()[5:],
+				'date': date.get_text(),
+				'source': source.get_text(),
+				'url': url,
+				'time': time.get_text()[6:-2]
+			}
+#			print(data)
+#			finance.insert_one(data)
 
+
+def Get_time(url):
+	wb_data = requests.get(url)
+	soup = BeautifulSoup(wb_data.content,'lxml')
+	times = soup.select('span.date')
+	return times
 
 Get_info()
